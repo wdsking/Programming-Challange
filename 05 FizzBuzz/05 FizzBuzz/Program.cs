0379aaaa71c02a05700faf5dbdfec89bb62a3609
wdsking <19kubaj98@gmail.com>
2018-07-27 02:15:34 +0200

    Update 1

---
+++ 05 FizzBuzz/05 FizzBuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _05_FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           for(int i = 1;i<101;i++)
+			{
+				if(i%15==0)
+				{
+					Console.WriteLine("FizzBuzz");
+					continue;
+				}
+				if(i%3==0)
+				{
+					Console.WriteLine("Fizz");
+					continue;
+				}
+				if(i%5==0)
+				{
+					Console.WriteLine("Buzz");
+					continue;
+				}
+				Console.WriteLine(i);
+			}
+			Console.ReadKey();
+        }
+    }
+}
